a17bed0214f00d74dc3db39ee97135ba7b7f16d3
David Ngungila <davidngungila@gmail.com>
2026-04-26 11:09:54 +0300

    Change admin page title to Dodoso La Duka La feedtan la Kisasa

--- resources/views/feedtan-survey/admin.blade.php
+++ resources/views/feedtan-survey/admin.blade.php
@@ -66,7 +66,7 @@
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"></path>
                                 </svg>
                             </div>
-                            <h1 class="text-gray-900 text-xl font-manrope font-bold">Bodi ya Utawala ya Uchunguzi wa Feedtan</h1>
+                            <h1 class="text-gray-900 text-xl font-manrope font-bold">Dodoso La Duka La feedtan la Kisasa</h1>
                         </div>
                     </div>
                     <div class="flex items-center space-x-4">
